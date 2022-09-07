a25f904e6338b1d4f31515c746ed46602bbbafdd
Shaik Anees <71702294+testadmin155@users.noreply.github.com>
2022-09-07 17:57:39 +0200

    Update Logintest.java

--- src/test/java/test/Logintest.java
+++ src/test/java/test/Logintest.java
@@ -8,6 +8,7 @@ public class Logintest {
 	 public void test1() {
 		 System.out.println("testcase2");
 		 System.out.println("Pushing teh code 2nd time");
+		 System.out.println("Pushing teh code 3rd time");
 	 }
 	 
 }
